a7e7869e608956f89a36225bf2f210b70bef1bc2
david-crocker <davidcrocker.code@gmail.com>
2017-01-15 12:04:31 -0600

    Config file added

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+app.config.from_pyfile('config.cfg')
 
 @app.route("/")
-def index():
+def index():	
 	return render_template('homepage.html')
 
 
